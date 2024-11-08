8c708c3e65ddc259edec37c8970b96ab27244ddb
Isaiah <ifbenzoni@gmail.com>
2024-11-08 00:48:33 -0500

    [logout]
    delete mapping instead of post for logout to work

--- maze_website/src/main/java/isaiah/maze_website/controllers/AccountController.java
+++ maze_website/src/main/java/isaiah/maze_website/controllers/AccountController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -69,7 +70,7 @@ public class AccountController {
 	 * @param response http servlet response for setting jwt
 	 * @return cookie in response + logout success message
 	 */
-	@PostMapping("/logout")
+	@DeleteMapping("/logout")
 	public ResponseEntity<String> logout(HttpServletResponse response) {
 		
 		response.addHeader("Set-Cookie", "userInfoJwt=; Path=/; HttpOnly; Max-Age=0; SameSite=None; Secure");
